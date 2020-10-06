157f1f880134e4fdb5ef7d6845e74e44215d1c89
alexraz0 <64764374+alexraz0@users.noreply.github.com>
2020-10-06 15:09:50 -0700

    Update saverr.cpp

--- saverr.cpp
+++ saverr.cpp
@@ -5,10 +5,39 @@
 #include <sys/stat/h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 using std::cout;
 using std::cerr;
 using std::endl;
 
-const auto REGION_SIZE = ...;
-const auto REGION_NAME
+const auto REGION_SIZE = 4096;
+const auto REGION_NAME = argv[1];
+const auto REGION_PERMS = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+
+int main(int argc, char *argv[])
+{
+  if(arg < 4 )
+    {
+      cerr<< "Usage: "<< argv[0] << "FILE PROG [ARGS] ... " << endl;
+      return EXIT_FAILURE;
+    }
+ 
+  auto fd = shm_open(REGION_NAME, O_CREAT | O_RDWR, REGION_PERMS);
+  if (fd < 0) 
+  {
+    perror("shm_open");
+    return EXIT_FAILURE;
+  }
+  
+  auto ok = ftruncate(fd, REGION_SIZE);
+  if(ok < 0)
+  {
+    perror("ftruncate");
+    return EXIT_FAILURE;
+  
+  
+  
+
+
+
